16fece9e35b6a6cf6e0a6a74ed3f50a12987e255
peace <astonreba@gmail.com>
2018-11-15 23:04:25 -0800

    Modify login_controllers.py file

--- api/controllers/login_controllers.py
+++ api/controllers/login_controllers.py
@@ -13,37 +13,35 @@ class LoginController(MethodView):
     """
     user_name = None
     password = None
-    user_id = None
     data = UserModel()
 
     def post(self):
         # to get post data
         post_data = request.get_json()
 
-        keys = ('user_name', 'password', 'user_id')
+        keys = ('user_name', 'password')
         if not set(keys).issubset(set(post_data)):
             return ResponseErrors.missing_fields(keys)
 
         try:
             self.user_name = post_data.get('user_name').strip()
             self.password = post_data.get('password').strip()
-            self.user_id = post_data.get('user_id')
         except AttributeError:
             return ResponseErrors.invalid_data_format()
 
-        if not self.user_name or not self.password or not self.user_id:
+        if not self.user_name or not self.password:
             return ResponseErrors.empty_data_fields()
 
         users = self.data.get_all_users()
         print(self.data.get_all_users())
 
         for single_user in users:
-            if self.user_id == single_user['user_id']:
+            if single_user['user_name'] == self.user_name and single_user['password'] == self.password:
 
                 response_object = {
                     'status': 'success',
                     'message': 'You are logged in',
-                    'logged_in_as': single_user
+                    'logged_in_as': single_user['user_name']
                 }
 
                 return jsonify(response_object), 200
