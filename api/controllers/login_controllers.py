24c5cc7f4ec2caeb6365c78867175058f96d5b05
peace <astonreba@gmail.com>
2018-11-19 07:08:25 -0800

    Add post request for login[Delivers #162051211]

---
+++ api/controllers/login_controllers.py
@@ -0,0 +1,63 @@
+"""
+This module looks at the user login
+"""
+import datetime
+
+from flask import request, jsonify
+from flask.views import MethodView
+from api.utils.validations import DataValidation
+from api.handlers.response_errors import ResponseErrors
+from api.auth.authenticate import Authenticate
+from api.models.database import DatabaseConnection
+from flask_jwt_extended import create_access_token
+
+
+class LoginController(MethodView):
+    """
+    User login class
+    """
+    data = DatabaseConnection()
+    val = DataValidation()
+    auth = Authenticate()
+
+    def post(self):
+        # to get post data
+        post_data = request.get_json()
+
+        keys = ('user_name', 'password')
+        if not set(keys).issubset(set(post_data)):
+            return ResponseErrors.missing_fields(keys)
+
+        try:
+            user_name = post_data.get("user_name").strip()
+            password = post_data.get("password").strip()
+        except AttributeError:
+            return ResponseErrors.invalid_data_format()
+
+        if not user_name or not password:
+            return ResponseErrors.empty_data_fields()
+
+        user = self.data.find_user_by_username(user_name)
+
+        if user and Authenticate.verify_password(password, user[5]):
+
+            response_object = {
+                'status': 'success',
+                'message': 'You are logged in',
+                "access_token": create_access_token(identity=user, expires_delta=datetime.timedelta(minutes=3600)),
+                'logged_in_as': str(user[1])
+                }
+
+            return jsonify(response_object), 200
+
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'User does not exist.'
+            }
+            return jsonify(response_object), 404
+
+
+
+
+
