2cb4977a428c75ea9a28ac36b95d354bb1f9c800
peace <astonreba@gmail.com>
2018-11-19 06:48:09 -0800

    Add method to hash password [Delivers #162051211]

---
+++ api/auth/authenticate.py
@@ -0,0 +1,20 @@
+from api.models.user_model import Users
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Authenticate:
+
+    myUser = Users()
+
+    @staticmethod
+    def hash_password(password):
+        """
+        method to hash password
+        :param password:
+        :return:
+        """
+        try:
+            return generate_password_hash(password, method="sha256")
+
+        except ValueError:
+            return False
