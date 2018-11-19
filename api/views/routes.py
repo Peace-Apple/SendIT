4095abdb0eae4e32f40544818930746aa6dab649
peace <astonreba@gmail.com>
2018-11-19 12:45:59 -0800

    Add route for posting an order

--- api/views/routes.py
+++ api/views/routes.py
@@ -3,6 +3,7 @@ Module to handle url requests
 """
 from api.controllers.signup_controllers import SignupController
 from api.controllers.login_controllers import LoginController
+from api.controllers.parcel_controllers import ParcelController
 
 
 class Routes:
@@ -17,19 +18,20 @@ class Routes:
         :param app:
         :return:
         """
-        app.add_url_rule('/api/v1/auth/signup', view_func=SignupController.as_view('register_user'),
+        app.add_url_rule('/api/v2/auth/signup/', view_func=SignupController.as_view('register_user'),
                          methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v1/auth/login', view_func=LoginController.as_view('login_user'),
+        app.add_url_rule('/api/v2/auth/login/', view_func=LoginController.as_view('login_user'),
                          methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v1/parcels/', view_func=ParcelController.as_view('make_delivery_order'),
+        app.add_url_rule('/api/v2/parcels/', view_func=ParcelController.as_view('make_delivery_order'),
                          methods=['POST'], strict_slashes=False)
-        app.add_url_rule('/api/v1/parcels/', view_func=ParcelController.as_view('get_all_orders'),
-                         methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/parcels/<int:parcel_id>', view_func=ParcelController.as_view('get_one_order'),
-                         methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/users/<int:user_id>/parcels/', view_func=ParcelController.as_view('get_user_parcels'),
-                         methods=['GET'], strict_slashes=False)
-        app.add_url_rule('/api/v1/parcels/<int:parcel_id>/cancel/',
-                         view_func=ParcelController.as_view('update_delivery_status'),
-                         methods=['PUT'], strict_slashes=False)
+        # app.add_url_rule('/api/v2/parcels/', view_func=ParcelController.as_view('get_all_orders'),
+        #                  methods=['GET'], strict_slashes=False)
+        # app.add_url_rule('/api/v2/parcels/<int:parcel_id>', view_func=ParcelController.as_view('get_one_order'),
+        #                  methods=['GET'], strict_slashes=False)
+        # app.add_url_rule('/api/v2/users/<int:user_id>/parcels/', view_func=ParcelController.as_view
+        #                 ('get_user_parcels'),
+        #                  methods=['GET'], strict_slashes=False)
+        # app.add_url_rule('/api/v2/parcels/<int:parcel_id>/cancel/',
+        #                  view_func=ParcelController.as_view('update_delivery_status'),
+        #                  methods=['PUT'], strict_slashes=False)
 
